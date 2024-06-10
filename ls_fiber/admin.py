28e219ab688f7b972944bf3d3be852c553edcad5
pan-dya <pandya.limawan@gmail.com>
2024-06-10 15:41:09 +0700

    Update admin.py

--- ls_fiber/admin.py
+++ ls_fiber/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-admin.site.register(JobDetail)
+from django.utils.timezone import localtime
+
+class JobDetailAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'get_local_timestamp')
+
+    def get_local_timestamp(self, obj):
+        return localtime(obj.timestamp).strftime('%Y-%m-%d %H:%M:%S %Z')
+
+    get_local_timestamp.short_description = 'Timestamp (Local Time)'
+
+admin.site.register(JobDetail, JobDetailAdmin)
 admin.site.register(Client)
 admin.site.register(ClientPIC)
 admin.site.register(ClientAlamat)
